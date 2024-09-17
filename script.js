3ff6fb3d56c07676ff2666a8f679248a86c8f9a8
Patricia <patricia.abadaranda@gmail.com>
2024-09-17 17:12:54 +0200

    Map and search box added

---
+++ script.js
@@ -0,0 +1,19 @@
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Search box
+let searchControl = new L.esri.Controls.Geosearch().addTo(map);
+let results = new L.LayerGroup().addTo(map);
+  searchControl.on('results', function(data){
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+  });
+
+
+
