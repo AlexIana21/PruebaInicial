7ad5edf9697a950126538b2fef54c2efb3f7edd0
Patricia <patricia.abadaranda@gmail.com>
2024-09-18 19:01:49 +0200

    Volcanoes, severe storms and earthquakes added

--- script.js
+++ script.js
@@ -40,6 +40,62 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    async function getEarthquakes() {
+        try {
+            const response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&category=earthquakes');
+            const data = await response.json();
+
+            console.log('Respuesta completa de la API:', data);
+
+            if (data.events && data.events.length > 0) {
+                displayEarthquakes(data.events);
+            } else {
+                console.warn('No se encontraron terremotos activos en la respuesta.');
+            }
+
+        } catch (error) {
+            console.error('Error fetching earthquakes:', error);
+        }
+    }
+
+    async function getVolcanoes() {
+        try {
+            const response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&category=volcanoes');
+            const data = await response.json();
+
+            console.log('Respuesta completa de la API:', data);
+
+            if (data.events && data.events.length > 0) {
+                displayVolcanoes(data.events);
+            } else {
+                console.warn('No se encontraron volcanes activos en la respuesta.');
+            }
+
+        } catch (error) {
+            console.error('Error fetching volcanoes:', error);
+        }
+    }
+
+    async function getSevereStorms() {
+        try {
+            const response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&category=severeStorms');
+            const data = await response.json();
+
+            console.log('Respuesta completa de la API:', data);
+
+            if (data.events && data.events.length > 0) {
+                displaySevereStorms(data.events);
+            } else {
+                console.warn('No se encontraron tormentas severas activos en la respuesta.');
+            }
+
+        } catch (error) {
+            console.error('Error fetching severe storms:', error);
+        }
+    }
+
+
+
     // Mostrar los incendios
     function displayWildfires(events) {
         events.forEach(event => {
@@ -80,29 +136,172 @@ document.addEventListener('DOMContentLoaded', function () {
         map.invalidateSize();
     }
 
+    // Mostrar los terremotos
+    function displayEarthquakes(events) {
+        events.forEach(event => {
+        
+            console.log('Detalles del evento:', event);
+
+            // el evento tiene geometrías y al menos una coordenada
+            if (event.geometry && event.geometry.length > 0) {
+                console.log(`Evento ${event.title} tiene geometrías:`, event.geometry);
+
+                const geometry = event.geometry[0]; // primera geometría
+                const coordinates = geometry.coordinates; // Coordenadas del evento
+
+                // Verificar si (array con dos elementos)
+                if (geometry.type === 'Point' && coordinates.length === 2) {
+                    const lat = coordinates[1]; 
+                    const lng = coordinates[0]; 
+
+                    // Crear el marcador solo si las coordenadas son válidas
+                    if (lat && lng) {
+                        let marker = L.marker([lat, lng], { 
+                            icon: getEventIcon('Earthquakes')
+                        }).addTo(map);
+
+                        marker.bindPopup(`<b>${event.title}</b><br>Fecha: ${new Date(geometry.date).toLocaleString()}`);
+                    } else {
+                        console.warn('Coordenadas inválidas para el evento:', event.title);
+                    }
+                } else {
+                    console.warn(`El evento ${event.title} no tiene una geometría de tipo Point con coordenadas válidas.`);
+                }
+            } else {
+                console.warn(`El evento ${event.title} no tiene geometrías disponibles.`);
+            }
+        });
+
+        // Refrescar el mapa
+        map.invalidateSize();
+    }
+
+        // Mostrar los volcanes
+        function displayVolcanoes(events) {
+            events.forEach(event => {
+            
+                console.log('Detalles del evento:', event);
+    
+                // el evento tiene geometrías y al menos una coordenada
+                if (event.geometry && event.geometry.length > 0) {
+                    console.log(`Evento ${event.title} tiene geometrías:`, event.geometry);
+    
+                    const geometry = event.geometry[0]; // primera geometría
+                    const coordinates = geometry.coordinates; // Coordenadas del evento
+    
+                    // Verificar si (array con dos elementos)
+                    if (geometry.type === 'Point' && coordinates.length === 2) {
+                        const lat = coordinates[1]; 
+                        const lng = coordinates[0]; 
+    
+                        // Crear el marcador solo si las coordenadas son válidas
+                        if (lat && lng) {
+                            let marker = L.marker([lat, lng], { 
+                                icon: getEventIcon('Volcanoes')
+                            }).addTo(map);
+    
+                            marker.bindPopup(`<b>${event.title}</b><br>Fecha: ${new Date(geometry.date).toLocaleString()}`);
+                        } else {
+                            console.warn('Coordenadas inválidas para el evento:', event.title);
+                        }
+                    } else {
+                        console.warn(`El evento ${event.title} no tiene una geometría de tipo Point con coordenadas válidas.`);
+                    }
+                } else {
+                    console.warn(`El evento ${event.title} no tiene geometrías disponibles.`);
+                }
+            });
+    
+            // Refrescar el mapa
+            map.invalidateSize();
+        }
+
+        // Mostrar las tormentas severas
+        function displaySevereStorms(events) {
+            events.forEach(event => {
+            
+                console.log('Detalles del evento:', event);
+    
+                // el evento tiene geometrías y al menos una coordenada
+                if (event.geometry && event.geometry.length > 0) {
+                    console.log(`Evento ${event.title} tiene geometrías:`, event.geometry);
+    
+                    const geometry = event.geometry[0]; // primera geometría
+                    const coordinates = geometry.coordinates; // Coordenadas del evento
+    
+                    // Verificar si (array con dos elementos)
+                    if (geometry.type === 'Point' && coordinates.length === 2) {
+                        const lat = coordinates[1]; 
+                        const lng = coordinates[0]; 
+    
+                        // Crear el marcador solo si las coordenadas son válidas
+                        if (lat && lng) {
+                            let marker = L.marker([lat, lng], { 
+                                icon: getEventIcon('SevereStorms')
+                            }).addTo(map);
+    
+                            marker.bindPopup(`<b>${event.title}</b><br>Fecha: ${new Date(geometry.date).toLocaleString()}`);
+                        } else {
+                            console.warn('Coordenadas inválidas para el evento:', event.title);
+                        }
+                    } else {
+                        console.warn(`El evento ${event.title} no tiene una geometría de tipo Point con coordenadas válidas.`);
+                    }
+                } else {
+                    console.warn(`El evento ${event.title} no tiene geometrías disponibles.`);
+                }
+            });
+    
+            // Refrescar el mapa
+            map.invalidateSize();
+        }
+
     // icono para el evento
     function getEventIcon(eventType) {
         switch (eventType) {
             case 'Wildfires':
               
                 return L.icon({
-                    iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png', 
-                    iconSize: [38, 38], 
+                    iconUrl: './img/fuego.png', 
+                    iconSize: [25, 25], 
                     iconAnchor: [22, 38], 
                     popupAnchor: [-3, -38] 
                 });
+
+            case 'Earthquakes':
+
+                return L.icon({
+                    iconUrl: './img/terremoto.png', 
+                    iconSize: [20, 20], 
+                    iconAnchor: [22, 38], 
+                    popupAnchor: [-3, -38] 
+                });
+            
+            case 'Volcanoes':
+                return L.icon({
+                    iconUrl: './img/volcan.png', 
+                    iconSize: [30, 30], 
+                    iconAnchor: [22, 38], 
+                    popupAnchor: [-3, -38] 
+                });
+            
+            case 'SevereStorms':
+            return L.icon({
+                iconUrl: './img/severestorms.png', 
+                iconSize: [30, 30], 
+                iconAnchor: [22, 38], 
+                popupAnchor: [-3, -38] 
+            });
+            
             default:
                 return L.icon({
-                    iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png', 
+                    iconUrl: '', 
                     iconSize: [38, 38],
                     iconAnchor: [22, 38],
                     popupAnchor: [-3, -38]
                 });
         }
     }
-
-    // Obtener los incendios cuando se cargue la página
-    getWildfires();
 
     // Filtrar eventos por el tipo seleccionado
     function displayFilteredEvents() {
@@ -118,6 +317,15 @@ document.addEventListener('DOMContentLoaded', function () {
             const eventType = checkbox.value;
             if (eventType === 'Wildfires') {
                 getWildfires();
+            }
+            else if (eventType === 'Earthquakes') {
+                getEarthquakes();
+            }
+            else if (eventType === 'Volcanoes') {
+                getVolcanoes();
+            }
+            else if (eventType === 'SevereStorms') {
+                getSevereStorms();
             }
         });
     }
